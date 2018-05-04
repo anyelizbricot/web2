be3d4f0a91a4300fef84b691953f599965db97d6
arico <anyelizbricot@gmail.com>
2018-05-04 17:13:37 -0500

    Add files via upload

---
+++ frontend/principal-vista.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sesion iniciada</title>
+
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <style>
+
+        body{
+            background-image: url(image/DSC_0707.jpg);
+        }
+      /*  YA LO TENGO EN LOS STYLES
+        .welcome{
+            width: 100%;
+            max-width: 600px;
+            margin: auto;
+            margin-top: 100px;
+            background: rgba(0,0,0,0.6);
+            text-align: center;
+            padding: 20px;
+        }
+      */
+        .welcome img{
+            width: 120px;
+            height: 120px;
+            text-align: center;
+        }
+
+        /*YA LO TENGO EN LOS STYLES
+        .welcome h1{
+            font-size: 50px;
+            color: white;
+            font-weight: 100;
+
+        }
+        */
+        /*YA LO TENGO EN LOS STYLES
+        .welcome a{
+            display: block;
+            margin-top: 40px;
+            font-size: 20px;
+            padding: 10px;
+            border: 1px solid white;
+        }*/
+      /*  YA LO TENGO EN LOS STYLES
+        .welcome a:hover{
+            color: black;
+            background: white;
+        }
+
+    */
+    </style>
+
+</head>
+<body>
+
+   <div class="welcome">
+      <!-- <img src="image/logo_magtimus.png" > -->
+        <h1>Bienvenido!</h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+   </div>
+
+</body>
+</html>
